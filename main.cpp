54bc887285c82b07067e01559e17e3b347bb793d
Andreas Moltumyr <kallexm@hotmail.com>
2020-09-11 17:54:02 +0200

    First success in compiling and running the test. Had to mess a bit with the libSlic3r code. Had to change 'and' to '&&' at one point. In addition had to add 'boost::placeholder::' infront of '_1' boost binding arguments to get it to work.

--- main.cpp
+++ main.cpp
@@ -6,14 +6,18 @@
 #include "ExPolygon.hpp"
 #include "BoundingBox.hpp"
 
+#include "boost/filesystem.hpp"
+
 int main()
 {
-    TriangleMesh tm;
+	std::string path = boost::filesystem::current_path().string();
+    std::string filename = "/../../test_object.stl";
+	filename = path + filename;
 
-    std::string filename = "test_file.stl";
-
+	TriangleMesh tm;
     try
     {
+		std::cout << filename << "\n";
         Slic3r::IO::STL::read(filename, &tm);
     }
     catch (...)
@@ -25,12 +29,12 @@ int main()
     Slic3r::TriangleMeshSlicer<Z> tm_slicer(&tm);
 
     std::vector<float> layers_z_vals{ 0.2, 0.4, 0.6, 0.8, 1.0 };
-    std::vector<ExPolygons>* tm_layers;
-    tm_slicer.slice(layers_z_vals, tm_layers);
+    std::vector<ExPolygons> tm_layers;
+    tm_slicer.slice(layers_z_vals, &tm_layers);
 
-    unsigned int inspect_layer_num = 2;
+    unsigned int inspect_layer_num = 4;
 
-	ExPolygons* choosen_layer = &(tm_layers->at(inspect_layer_num - 1));
+	ExPolygons* choosen_layer = &(tm_layers.at(inspect_layer_num - 1));
 	for (ExPolygons::const_iterator iter = choosen_layer->begin(); iter != choosen_layer->end(); iter++)
 	{
 		BoundingBox bb_choosen_layer = iter->bounding_box();
@@ -38,6 +42,5 @@ int main()
 		std::cout << "BoundingBox end   = (" << bb_choosen_layer.max.x << ", " << bb_choosen_layer.max.y << ")\n\n";
 	}
     
-
     return 0;
 }
